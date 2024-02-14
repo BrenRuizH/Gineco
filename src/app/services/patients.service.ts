f7409e7b846c17acfb3e5f85d1a176d0e44f52ee
Bren Ruiz <ruizhernandezbrendac@gmail.com>
2024-02-13 19:48:45 -0600

    2024-02-13 Header Menu

--- src/app/services/patients.service.ts
+++ src/app/services/patients.service.ts
@@ -8,14 +8,14 @@ export class PatientsService {
   public menu: any = [
     {
       title: 'Doctors',
-      icon: 'mdi mdi-gauge',
+      icon: 'mdi mdi-stethoscope',
       submenu: [
         { title: 'Doctor´s Data', url: 'dashboard/doctors-data' }
       ]
     },
     {
       title: 'Patients',
-      icon: 'mdi mdi-gauge',
+      icon: 'mdi mdi-account',
       submenu: [
         { title: 'Create Patient', url: 'dashboard/create-patient' },
         { title: 'Patient´s Data', url: 'dashboard/patients-data' }
@@ -23,7 +23,7 @@ export class PatientsService {
     },
     {
       title: 'Medical History',
-      icon: 'mdi mdi-gauge',
+      icon: 'mdi mdi-content-paste',
       submenu: [
         { title: 'New Record', url: 'dashboard/new-record' },
         { title: 'Patient´s Record', url: 'dashboard/patients-record' }
