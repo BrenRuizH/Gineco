078f61c08c742b24ea9580a7a24524dac2696ab4
Bren Ruiz <ruizhernandezbrendac@gmail.com>
2024-02-14 15:54:22 -0600

    2024-02-14 Function Post Method

--- src/app/services/patients.service.ts
+++ src/app/services/patients.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { apiServer } from '../apiServer';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -36,7 +37,11 @@ export class PatientsService {
   ]
   constructor(private http: HttpClient) { }
 
-  postMethod(url: any, body: any) {
+  postMethod(url: any, body: any): Observable<any> {
     return this.http.post(`${this.url}${url}`, body);
+  }
+
+  getMethod(url: any): Observable<any> {
+    return this.http.get(`${this.url}${url}`);
   }
 }
