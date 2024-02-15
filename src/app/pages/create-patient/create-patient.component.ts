6353bb510b409eb719bfcf9bac9f9ff576621145
Bren Ruiz <ruizhernandezbrendac@gmail.com>
2024-02-14 18:10:57 -0600

    2024-02-14 Sweet Alert

--- src/app/pages/create-patient/create-patient.component.ts
+++ src/app/pages/create-patient/create-patient.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { PatientsService } from 'src/app/services/patients.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-create-patient',
@@ -14,18 +15,35 @@ export class CreatePatientComponent {
   constructor (public patientsService: PatientsService, public router: Router) {}
 
   altaPaciente() {
-    let formData = new FormData();
-    formData.append('nompaciente', this.patients.nompaciente);
-    formData.append('edadpaciente', this.patients.edadpaciente);
-    formData.append('telpaciente', this.patients.telpaciente);
-    formData.append('dirpaciente', this.patients.dirpaciente);
-    console.log(formData);
+    Swal.fire({
+      title: "¿Desea regitrar al paciente?",
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: "Registrar",
+      denyButtonText: `No acepto`
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        
+        let formData = new FormData();
+        formData.append('nompaciente', this.patients.nompaciente);
+        formData.append('edadpaciente', this.patients.edadpaciente);
+        formData.append('telpaciente', this.patients.telpaciente);
+        formData.append('dirpaciente', this.patients.dirpaciente);
+        console.log(formData);
 
-    this.patientsService.postMethod('altaPaciente.php', formData).subscribe((event: any) =>{
-      console.log(event);
-      if (event.status == 'success') {
-        this.router.navigate(['/dashboard/new-record']);
+        this.patientsService.postMethod('altaPaciente.php', formData).subscribe((event: any) =>{
+          console.log(event);
+          Swal.fire("¡Registrado!", "", "success");
+          if (event.status == 'success') {
+            this.router.navigate(['/dashboard/new-record']);
+          }
+        })
+      } else if (result.isDenied) {
+        Swal.fire("Ups!", "", "info");
       }
-    })
+    });
+
+    
   }
 }
