64904277801ce69edc22170acec53927678f691b
Bren Ruiz <ruizhernandezbrendac@gmail.com>
2024-02-19 18:10:35 -0600

    2024-02-19 Get Patient Information

--- src/app/pages/new-record/new-record.component.ts
+++ src/app/pages/new-record/new-record.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PatientsService } from 'src/app/services/patients.service';
 
 @Component({
   selector: 'app-new-record',
   templateUrl: './new-record.component.html',
   styleUrls: ['./new-record.component.css']
 })
-export class NewRecordComponent {
+export class NewRecordComponent implements OnInit{
+  mostrar: boolean = false;
+  patients: any[] = []
 
+  constructor(private patientsService: PatientsService) {}
+
+  ngOnInit(): void {
+      this.getPatientes();
+  }
+
+  getPatientes() {
+    this.patientsService.getMethod('ObtenerPacientes.php').subscribe((data) => {
+      this.patients = data.document;
+      console.log(this.patients);
+    })
+  }
 }
