c7111a57e85e82c0feaa080fa30033c0a2164443
Bren Ruiz <ruizhernandezbrendac@gmail.com>
2024-02-19 23:08:48 -0600

    2024-02-19 Registration History

--- src/app/pages/new-record/new-record.component.ts
+++ src/app/pages/new-record/new-record.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { PatientsService } from 'src/app/services/patients.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-new-record',
@@ -17,20 +19,54 @@ export class NewRecordComponent implements OnInit{
     fechahistorial: this.fecha
   };
 
-  constructor(private patientsService: PatientsService) {}
+  constructor(private patientsService: PatientsService, private router: Router) {}
 
   ngOnInit(): void {
-      this.getPatientes();
+      this.getPatients();
   }
 
-  getPatientes() {
+  getPatients() {
     this.patientsService.getMethod('ObtenerPacientes.php').subscribe((data) => {
       this.patients = data.document;
       console.log(this.patients);
     })
   }
 
-  altaHistoriarial(){
-    console.log(this.newhistorial);
+  altaHistorial() {
+    Swal.fire({
+      title: "¿Desea regitrar el historial?",
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: "Registrar",
+      denyButtonText: `No acepto`
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        
+        let formData = new FormData();
+        formData.append('pesohistorial', this.newhistorial.pesohistorial);
+        formData.append('tallahistorial', this.newhistorial.tallahistorial);
+        formData.append('fchistorial', this.newhistorial.fchistorial);
+        formData.append('frhistorial', this.newhistorial.frhistorial);
+        formData.append('ahhistorial', this.newhistorial.ahhistorial);
+        formData.append('apnphistorial', this.newhistorial.apnphistorial);
+        formData.append('hemotipohistorial', this.newhistorial.hemotipohistorial);
+        formData.append('alergiashistorial', this.newhistorial.alergiashistorial);
+        formData.append('apphistorial', this.newhistorial.apphistorial);
+        formData.append('citahistorial', this.newhistorial.citahistorial);
+        formData.append('idpaciente', this.newhistorial.idpaciente);
+        formData.append('diagnostico', this.newhistorial.diagnostico);
+        formData.append('fechahistorial', this.newhistorial.fechahistorial);
+
+        this.patientsService.postMethod('NuevoHistorial.php', formData).subscribe((event: any) =>{
+          Swal.fire("¡Registrado!", "", "success");
+          if (event.status == 'success') {
+            this.router.navigate(['/dashboard/new-record']);
+          }
+        })
+      } else if (result.isDenied) {
+        Swal.fire("Ups!", "", "info");
+      }
+    });
   }
 }
