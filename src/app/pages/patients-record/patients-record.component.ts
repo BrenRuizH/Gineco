286f0ac1805992cb29e1c33db73e3f87a28b6dd6
Bren Ruiz <ruizhernandezbrendac@gmail.com>
2024-02-21 21:04:52 -0600

    2024-02-21 Subscribe to Get History

--- src/app/pages/patients-record/patients-record.component.ts
+++ src/app/pages/patients-record/patients-record.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PatientsService } from 'src/app/services/patients.service';
 
 @Component({
   selector: 'app-patients-record',
   templateUrl: './patients-record.component.html',
   styleUrls: ['./patients-record.component.css']
 })
-export class PatientsRecordComponent {
+export class PatientsRecordComponent implements OnInit{
 
+  newHistoriales: any[] = [];
+
+  constructor(public patientService: PatientsService) {}
+
+  ngOnInit(): void {
+    this.obtenerHistoriales();
+  }
+
+  obtenerHistoriales() {
+    this.patientService.obtenerHistoriales()
+    .subscribe((resp: any) => {
+      this.newHistoriales = resp;
+      console.log(this.newHistoriales);
+    })
+  }
 }
