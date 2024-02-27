cc1858065cb1f7bbc957ae1cbe9d4c22f16eaf40
Bren Ruiz <ruizhernandezbrendac@gmail.com>
2024-02-27 13:40:51 -0600

    2024-02-27 File Subscription

--- src/app/pages/record/record.component.ts
+++ src/app/pages/record/record.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PatientsService } from 'src/app/services/patients.service';
 
 @Component({
   selector: 'app-record',
   templateUrl: './record.component.html',
   styleUrls: ['./record.component.css']
 })
-export class RecordComponent {
+export class RecordComponent implements OnInit{
 
   expedientes: any = {};
-  constructor(private activetedRouter: ActivatedRoute) {
+  exp: any[] = [];
+
+  constructor(private patientsService: PatientsService, private activetedRouter: ActivatedRoute) {
     this.activetedRouter.params.subscribe(paramas => {
       this.expedientes = paramas['id'];
       console.log(this.expedientes);
-    })
+    });
+  }
+
+  ngOnInit(): void {
+      this.obtenerExpedientes();
+  }
+
+  obtenerExpedientes() {
+    this.patientsService.obtenerExpedientes(this.expedientes)
+    .subscribe((resp: any) => {
+      this.exp = resp;
+      console.log(this.exp);
+    });
   }
 }
